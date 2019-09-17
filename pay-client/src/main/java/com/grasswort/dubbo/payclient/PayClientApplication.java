b61867b8030564b1086152c363e772cafb2b13f3
xuliangliang <835547206@qq.com>
2019-09-17 19:10:28 +0800

    取消一些不必要的配置

--- pay-client/src/main/java/com/grasswort/dubbo/payclient/PayClientApplication.java
+++ pay-client/src/main/java/com/grasswort/dubbo/payclient/PayClientApplication.java
@@ -1,13 +1,12 @@
 package com.grasswort.dubbo.payclient;
 
-import com.alibaba.nacos.api.annotation.NacosProperties;
-import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@NacosPropertySource(dataId = "example", autoRefreshed = true)
+@NacosPropertySource(dataId = "example", groupId = "${nacos.config.group}", autoRefreshed = true)
+//@NacosPropertySource(dataId = "application.properties", groupId = "${nacos.config.group}")
 public class PayClientApplication {
 
     public static void main(String[] args) {
